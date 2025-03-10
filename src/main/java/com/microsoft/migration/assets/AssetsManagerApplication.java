3650f19ff3a38e79b639564ff56e45221f3e8b03
allxiao <arieshout@gmail.com>
2025-03-10 09:50:55 +0800

    Initialize project from Spring starter

---
+++ src/main/java/com/microsoft/migration/assets/AssetsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.microsoft.migration.assets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetsManagerApplication.class, args);
+	}
+
+}
